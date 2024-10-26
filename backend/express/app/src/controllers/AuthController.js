45f5f3abf1da8b5505f383d789ad666a72c55dec
kouichi310 <sirorabi516@gmail.com>
2024-10-26 14:21:49 +0900

    認証認可を実装

---
+++ backend/express/app/src/controllers/AuthController.js
@@ -0,0 +1,13 @@
+const AuthService = require("../services/AuthService");
+
+module.exports = class AuthController {
+  static async login(request, response, next) {
+    try {
+      const { body } = request;
+      const authInfo = await new AuthService().login(body);
+      response.json(authInfo);
+    } catch (e) {
+      next(e);
+    }
+  }
+};
--- backend/express/app/src/router.js
+++ backend/express/app/src/router.js
@@ -1,8 +1,15 @@
 const { Router } = require("express");
 const UsersController = require("./controllers/UsersController.js");
+const AuthController = require("./controllers/AuthController.js");
+
+const verifyAccessToken = require("./middlewares/verifyAccessToken.js");
 
 const router = new Router();
 
-router.get("/users", UsersController.fetchAll);
+// Auth
+router.post("/login", AuthController.login);
+
+// Users
+router.get("/users", verifyAccessToken, UsersController.fetchAll);
 
 module.exports = router;
